a16ea26efdb6e54b0626d28f6f81f32356dd9ff1
Bhargav <bhargavkadali39@gmail.com>
2021-10-14 21:24:38 +0530

    Update dynamic-input.py

--- Sub-branches/dynamic-input.py
+++ Sub-branches/dynamic-input.py
@@ -1,9 +1,10 @@
 n = int(input("Enter number of elements :"))
 
+# list with some default words for the game to be played asap.
 a = ['Gas','Food','Gym','Health']
 
 # Below line read inputs from user using map()
-
+# which consists of list converter function with map() helped to place all user given words inside the xlsx file.
 b = list(map(str,input("\nEnter the words in a single go: ").strip().split()))[:n]
 
 print("\n List is -", a)
