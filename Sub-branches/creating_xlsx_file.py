ae5376e6fcd5813e75a3796db6b9855aa76ca11d
Bhargav <bhargavkadali39@gmail.com>
2021-10-14 21:18:07 +0530

    Update creating_xlsx_file.py

--- Sub-branches/creating_xlsx_file.py
+++ Sub-branches/creating_xlsx_file.py
@@ -1,1 +1,35 @@
 
+import xlsxwriter
+from time import sleep
+
+# Create a workbook and add a worksheet.
+name = str(input('Enter name for the xlsx file:'))
+name = name+'.xlsx'
+print(name)
+
+workbook = xlsxwriter.Workbook(name)
+worksheet = workbook.add_worksheet()
+
+# Some data we want to write to the worksheet.
+expenses = ['Rent', 'Gas', 'Food', 'Gym']
+
+
+
+# Start from the first cell. Rows and columns are zero indexed.
+row = 0
+col = 0
+
+# Iterate over the data and write it out row by row.
+for item in expenses:
+    worksheet.write(row, col, item)
+    row += 1
+
+sleep(1)
+print('Items added')
+
+sleep(0.5)
+
+workbook.close()
+print('workbook created')
+
+
