01b64a0f4606cc8cdd74b397610b93c4ad10a95d
Bhargav <bhargavkadali39@gmail.com>
2021-10-16 22:52:26 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,3 +2,8 @@
 # Noting to see here yet
 
 # full code will be available once this repo is completely done for.
+
+# or else you can check the simpler version which is already available 
+# link: https://github.com/BhargavKadali39/hangman-explained-using-python/blob/main/simple-version.py
+
+# peace
